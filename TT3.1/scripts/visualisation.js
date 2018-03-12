0e1a64b8109d16eba32c17251b29af9f9d8aa04f
cdilga <c.dilger1@gmail.com>
2018-03-12 15:42:20 +1100

    Complete week 3 tasks

--- TT3.1/scripts/visualisation.js
+++ TT3.1/scripts/visualisation.js
@@ -1,47 +1,97 @@
 function init() {
   var w = 500;
-  var h = 100;
-  var dataset;
+  var h = 400;
 
-  var rowConverter = function(d) {
+  var padding = 20;
+  var hPadding = padding;
+  var hRightPadding = hPadding;
+  var hLeftPadding = hPadding;
+  var wPadding = padding;
+  var wLeftPadding = wPadding;
+  var wRightPadding = wPadding + 50;
+  var dataset = [
+  [140, 50],
+  [210, 23],
+  [12, 90],
+  [45, 32],
+  [12, 9],
+  [100, 5],
+  [60, 60]
+  ];
 
-    return {
-      Test: parseFloat(d.Test)
-    };
-  }
+  var xScale = d3.scaleLinear()
+    .domain([d3.min(dataset, function (d) {
+      return d[0];
+    }),
+    d3.max(dataset, function (d) {
+      return d[0];
+    })])
+    .range([wLeftPadding, w - wRightPadding]);
 
-  d3.csv("data/random-data.csv", rowConverter, function(data, error) {
-    if (data.length === 0) {
-      d3.select("#chart").append("p")
-        .text("Data failed to load");
+  var yScale = d3.scaleLinear()
+    .domain([d3.min(dataset, function (d) {
+      return d[1];
+    }),
+    d3.max(dataset, function (d) {
+      return d[1];
+    })])
+    .range([h - hLeftPadding, hRightPadding]);
+
+  var xAxis = d3.axisBottom()
+    .ticks(10)
+    .scale(xScale);
+  var yAxis = d3.axisLeft()
+    .scale(yScale);
+
+
+
+  var svg = d3.select("#chart")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h);
+  svg.selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("circle")
+  .attr("cx", function(d, i) {
+    return xScale(d[0]);
+  })
+  .attr("cy", function(d) {
+    return yScale(d[1]);
+  })
+  .attr("r", 5)
+  .attr("fill", function(d, i) {
+    if (i == 4) {
+      return "red";
     }
-    dataset = data;
-    console.log(dataset);
-
-    barChart(dataset);
+    return "slategrey";
   });
 
-  function barChart() {
-    var svg = d3.select("#chart")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+  svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+      return "(" + d[0] + ", " + d[1] + ")";
+    })
+    .attr("x", function(d, i) {
+      return xScale(d[0]) + 5;
+    })
+    .attr("y", function(d) {
+      return yScale(d[1]) + 10;
+    })
+    .attr("fill", "green");
 
-    svg.selectAll("rect")
-      .data(dataset)
-      .enter()
-      .append("rect")
-      .attr("x", function(d, i) {
-        return i * (w / dataset.length);
-      })
-      .attr("y", function(d) {
-        return h - d.Test;
-      })
-      .attr("width", w / dataset.length - 1)
-      .attr("height", function(d) {
-        return d.Test;
-      });
-  }
+  svg.append("g")
+    .attr("transform", "translate (0, " + (h - padding) + ")")
+    .attr("class", "axis")
+    .call(xAxis);
+  svg.append("g")
+    .attr("transform", "translate (" + padding + ", 0)")
+    .attr("class", "axis")
+    .call(yAxis);
+
+
 }
 
 window.onload = init;
