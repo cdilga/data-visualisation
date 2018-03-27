4ca39eb9d5f6a5b6c40c84e88c108129a869e95b
cdilga <c.dilger1@gmail.com>
2018-03-27 21:44:06 +1100

    Complete 5.2

---
+++ TT5.2/scripts/vis.js
@@ -0,0 +1,67 @@
+function init () {
+
+  var w = 500;
+  var h = 100;
+  var data = true;
+  var data1 = [14, 5, 26, 23, 9, 90, 45, 32, 12, 4];
+  var data2 = [12, 5, 3, 16, 6, 1, 2, 6];
+  var currdata = data1;
+
+  d3.select("#update")
+    .on("click", function() {
+      data = !data;
+      if(data) {
+        currdata = data1;
+      } else {
+        currdata = data2;
+      }
+
+      svg.selectAll("rect")
+        .data(currdata)
+        .transition()
+        .delay(function(d, i) {
+          return i/currdata.length * 500;
+        })
+        .duration(400)
+        .ease(d3.easeCubicInOut)
+        .attr("x", function(d, i) {
+          return xScale(i);
+        })
+        .attr("y", function(d) {
+          return h - d;
+        })
+    });
+    var yScale = d3.scaleLinear()
+      .domain([0, d3.max(currdata)])
+      .range([0, h]);
+
+    var xScale = d3.scaleBand()
+      .domain(d3.range(currdata.length))
+      .rangeRound([0,w])
+      .paddingInner(0.05);
+
+    var svg = d3.select("body")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+    svg.selectAll("rect")
+      .data(currdata)
+      .enter()
+      .append("rect");
+
+    svg.selectAll("rect")
+      .attr("x", function(d, i) {
+        return xScale(i);
+      })
+      .attr("y", function(d) {
+        return h - d;
+      })
+      .attr("width", xScale.bandwidth())
+      .attr("height", function(d) {
+        return yScale(d);
+      })
+      .attr("fill", "red");
+}
+
+window.onload = init;
